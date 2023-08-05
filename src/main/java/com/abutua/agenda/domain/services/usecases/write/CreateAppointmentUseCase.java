0b6a1e7a351b3331775432a6397a9ec3adc04f76
Glauco Todesco <glauco.todesco@abutua.com>
2023-08-04 22:29:08 -0300

    :bug: fix: Create an appoitment atomically

--- src/main/java/com/abutua/agenda/domain/services/usecases/write/CreateAppointmentUseCase.java
+++ src/main/java/com/abutua/agenda/domain/services/usecases/write/CreateAppointmentUseCase.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.abutua.agenda.domain.entities.Appointment;
 import com.abutua.agenda.domain.entities.AppointmentType;
@@ -44,7 +45,6 @@ public class CreateAppointmentUseCase {
     @Autowired
     private SearchProfessionalAvailabiltyTimesUseCase searchProfessionalAvailabiltyTimesUseCase;
 
-
     public Appointment executeUseCase(Appointment appointment) {
 
         checkAppointmentTypeExistsOrThrowsException(appointment.getAppointmentType());
@@ -54,51 +54,58 @@ public class CreateAppointmentUseCase {
         checkProfessionalIsActiveOrThrowsException(professional);
         checkAssociationBetweenProfessionalAndAreaOrThrowsException(professional, appointment.getArea());
         checkProfessionalCanCreateAppointmentAtDateAndTimeOrThrowsException(professional, appointment);
-        checkProfessionalHasAvailableScheduleOrThorwsException(professional, appointment);
-
+      
         checkAppointmentIsNowOrFutureOrThrowsException(appointment.getDate(), appointment.getStartTime());
 
         Client client = getClientIfExistsOrThrowsException(appointment.getClient());
-        checkClientCanCreateAppointmentAtDateAndTimeOrThrowsException(client, appointment);
 
+        return saveAppointmentAtomically(appointment, professional, client);
+
+    }
+
+    @Transactional
+    private Appointment saveAppointmentAtomically(Appointment appointment, Professional professional, Client client) {
+        checkProfessionalHasAvailableScheduleOrThorwsException(professional, appointment);
+        checkClientCanCreateAppointmentAtDateAndTimeOrThrowsException(client, appointment);
         return this.appointmentRepository.save(appointment);
     }
 
-    
-    private void checkProfessionalHasAvailableScheduleOrThorwsException(Professional professional, Appointment appointment) {
-       var timeSlots = this.searchProfessionalAvailabiltyTimesUseCase.executeUseCase(professional.getId(), appointment.getDate());
+    private void checkProfessionalHasAvailableScheduleOrThorwsException(Professional professional,
+            Appointment appointment) {
+        var timeSlots = this.searchProfessionalAvailabiltyTimesUseCase.executeUseCase(professional.getId(),
+                appointment.getDate());
 
-       if(timeSlots.isEmpty()){
-           throw new BusinessException("O professional não trabalha na data selecionada.");
-       }
-       else{
-            var timeSlot = timeSlots.stream().filter( ts -> ts.getStartTime().toLocalTime().equals(appointment.getStartTime())  &&
-                                                            ts.getEndTime().toLocalTime().equals(appointment.getEndTime())
-                                                    ).findFirst();
-            
-            if(timeSlot.isEmpty()){
+        if (timeSlots.isEmpty()) {
+            throw new BusinessException("O professional não trabalha na data selecionada.");
+        } else {
+            var timeSlot = timeSlots.stream()
+                    .filter(ts -> ts.getStartTime().toLocalTime().equals(appointment.getStartTime()) &&
+                            ts.getEndTime().toLocalTime().equals(appointment.getEndTime()))
+                    .findFirst();
+
+            if (timeSlot.isEmpty()) {
                 throw new BusinessException("O professional não trabalha no horário selecionado.");
             }
-       }
-    }
-
-
-    private void checkProfessionalCanCreateAppointmentAtDateAndTimeOrThrowsException(Professional professional,  Appointment appointment) {
-         if(this.appointmentRepository.existsOpenOrPresentAppointmentsForProfessional( professional, 
-                                                                                appointment.getDate(), 
-                                                                                appointment.getStartTime(),
-                                                                                appointment.getEndTime())){
-              throw new BusinessException("O profissional possui agendamentos em aberto para o dia e horário selecionado.");
         }
     }
 
+    private void checkProfessionalCanCreateAppointmentAtDateAndTimeOrThrowsException(Professional professional,
+            Appointment appointment) {
+        if (this.appointmentRepository.existsOpenOrPresentAppointmentsForProfessional(professional,
+                appointment.getDate(),
+                appointment.getStartTime(),
+                appointment.getEndTime())) {
+            throw new BusinessException(
+                    "O profissional possui agendamentos em aberto para o dia e horário selecionado.");
+        }
+    }
 
     private void checkClientCanCreateAppointmentAtDateAndTimeOrThrowsException(Client client, Appointment appointment) {
-        if(this.appointmentRepository.existsOpenOrPresentAppointmentsForClient( client, 
-                                                                                appointment.getDate(), 
-                                                                                appointment.getStartTime(),
-                                                                                appointment.getEndTime())){
-              throw new BusinessException("O cliente possui agendamentos em aberto para o dia e horário selecionado.");
+        if (this.appointmentRepository.existsOpenOrPresentAppointmentsForClient(client,
+                appointment.getDate(),
+                appointment.getStartTime(),
+                appointment.getEndTime())) {
+            throw new BusinessException("O cliente possui agendamentos em aberto para o dia e horário selecionado.");
         }
     }
 
