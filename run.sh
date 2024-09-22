8795ceb55207adf70c1859df2591bfb56ad32c79
Glauco Todesco <glauco.todesco@gmail.com>
2024-09-21 22:01:00 -0300

    teste

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ cd /home/ec2-user/agenda
 docker stop agenda
 docker rm agenda
 docker build -t agenda-backend .
-docker run -d --name agenda -p 8080:8080 agenda-backend
+docker run --restart unless-stopped -d --name agenda -p 8080:8080  agenda-backend 
 
---
+++ src/main/java/com/abutua/agenda/web/resources/Hello.java
@@ -0,0 +1,18 @@
+package com.abutua.agenda.web.resources;
+
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@RestController
+public class Hello {
+
+    @GetMapping("hello")
+    public String getMethodName() {
+        return "V1.0";
+    }
+    
+    
+}
