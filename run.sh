04cfdc6421f0e0e4e8f8831e4d3b50870a63d3ab
glaucotodesco <glauco.todesco@gmail.com>
2024-10-10 18:40:09 -0300

    Add .env

--- run.sh
+++ run.sh
@@ -4,5 +4,8 @@ cd /home/ec2-user/agenda
 docker stop agenda
 docker rm agenda
 docker build -t agenda-backend .
-docker run --restart unless-stopped -d --name agenda -p 8080:8080  agenda-backend 
+sudo aws s3 cp s3://abutua-docker-envs/agenda.env .
+docker run --env-file ./agenda.env  --restart unless-stopped -d --name agenda -p 8080:8080  agenda-backend 
+cd ..
+sudo rm -rf agenda
 
